b444c70cc06a912803d3860c40d14746abbb45a6
ivailo-kolarov <kolarov.ivailo@gmail.com>
2015-02-11 19:31:12 +0200

    First upload

---
+++ C#PartTwo/HomeWork/01.Arrays/01.Arrays/02.CompareArrays/CompareArrays.cs
@@ -0,0 +1,19 @@
+﻿using System;
+//Write a program that reads two integer arrays from the console and compares them element by element.
+
+class Program
+{
+
+    static void Main()
+    {
+        int[] arrayOne = new int[20];
+        int[] arrayTwo = new int[20];
+        
+        for (int i = 0; i < 20; i++)
+        {
+            
+        }
+    }
+
+}
+
